24785b3a90c0070ba5b673d5589da9a58739c11f
Li Den <malivenlipton@gmail.com>
2024-11-14 15:51:34 +0300

    main

---
+++ tests/settings.go
@@ -0,0 +1,7 @@
+package tests
+
+var Port = 7540
+var DBFile = "../scheduler.db"
+var FullNextDate = true
+var Search = true
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZCI6IiJ9.sgOfht2gj7-D_-HdrvnboC_ehEq-UGdzQQpl3devG_s`
